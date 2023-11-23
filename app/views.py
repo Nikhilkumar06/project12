1ae9e25a2abf8b2e4d6c62a51761908fdc1a8981
Nikhilkumar06 <nikhiltupakula255@gmail.com>
2023-11-23 12:11:58 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def pspk(request):
+    return render(request,'pspk.html')
+
+def suriya(request):
+    return render(request,'suriya.html')
+
+def balidan(request):
+    return render(request,'balidan.html')
